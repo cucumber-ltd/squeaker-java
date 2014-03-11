29bca4a4617a4a113aef7499ce308ce77b45e279
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2014-03-11 13:45:15 +0000

    Added bare bones squeaker example for Cucumber-Java

---
+++ src/test/java/squeaker/RunCukesTest.java
@@ -0,0 +1,10 @@
+package squeaker;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(monochrome = true)
+public class RunCukesTest {
+}
---
+++ src/test/java/squeaker/Stepdefs.java
@@ -0,0 +1,43 @@
+package squeaker;
+
+import cucumber.api.java.After;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.Assert.fail;
+
+public class Stepdefs {
+
+    private final WebDriver browser = new FirefoxDriver();
+
+    @Given("^I'm not logged in$")
+    public void I_m_not_logged_in() throws Throwable {
+        // Of course we're not logged in
+    }
+
+    @When("^I visit the homepage$")
+    public void I_visit_the_homepage() throws Throwable {
+        browser.get("http://squeaker.heroku.com/");
+    }
+
+    @Then("^I should see \"([^\"]*)\"$")
+    public void I_should_see(String expectedText) throws Throwable {
+        String actualText = browser.findElement(By.tagName("body")).getText();
+        assertContains(expectedText, actualText);
+    }
+
+    private void assertContains(String expectedText, String actualText) {
+        if (!actualText.contains(expectedText)) {
+            fail(String.format("Couldn't find [%s] in [%s]", expectedText, actualText));
+        }
+    }
+
+    @After
+    public void closeBrowser() {
+        browser.close();
+    }
+}
